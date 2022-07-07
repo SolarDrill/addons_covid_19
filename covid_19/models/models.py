4519591b45b2bbe085657311f49931559e640912
SolarDrill <ramonsvnpp@gmail.com>
2022-07-07 08:57:01 -0400

    Model added

--- covid_19/models/models.py
+++ covid_19/models/models.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class covid_19(models.Model):
-#     _name = 'covid_19.covid_19'
-#     _description = 'covid_19.covid_19'
+class covid_19(models.Model):
+    _name = 'covid.covid_19'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
+    source = fields.Char(string='Source', required=True)
+    date = fields.datetime(string='Date Time', required=True, default=fields.datetime.now())
+    country_id = fields.Many2one(comodel_name="res.country", string="", required=True)
+    infected = fields.Integer(string="Infected", required=True, default=0)
+    recovered = fields.Integer(string="Recovered", required=True, default=0)
+    deceased = fields.Integer(string="Deceased", required=True, default=0)
+
+
 #     @api.depends('value')
 #     def _value_pc(self):
 #         for record in self:
